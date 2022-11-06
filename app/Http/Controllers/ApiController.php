e03531a9d4945558eede9c8b5dba37bec37ab1a7
Sebastian99fuentes <80362481+Sebastian99fuentes@users.noreply.github.com>
2022-11-06 13:58:51 -0500

    Full crud back

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    public function users(Request $request){
+
+     $users =User::all();
+     return response()->json($users);
+    }
+    public function login(Request $request){
+
+        
+    }
+}
